d1a341ea1df79da36997076309e49c45999669d1
betharavikiran <betha.ravikiran@gmail.com>
2019-08-27 17:22:40 +0530

    Object export might not work....

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import loginCall from './src/methods/loginCall';
 import { getConfig } from './src/utils/getConfig';
-import { KarmaVendor } from './src/KarmaVendor';
+//import { KarmaVendor } from './src/KarmaVendor';
 
 function getKarmaVendorInstance(env, loginId, password){
    console.log(env);
@@ -11,13 +11,7 @@ function getKarmaVendorInstance(env, loginId, password){
    const loginResult = loginCall(config, loginId, password);
    console.log(loginResult);
    const { status, jwt, userId } = loginResult;
-   if(status==="SUCCESS"){
-       const { HTTP_ENDPOINT, WS_ENDPOINT } = config;
-       return new KarmaVendor(HTTP_ENDPOINT,WS_ENDPOINT, userId, jwt);
-   }
-   else{
-       return null;
-   }
+   return true;
 }
 
 module.exports = getKarmaVendorInstance;
