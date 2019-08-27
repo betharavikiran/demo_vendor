afd16be6ae70e8442c61ed8fe29ad171a71f356a
betharavikiran <betha.ravikiran@gmail.com>
2019-08-27 14:45:42 +0530

    Export Karma Instance

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import loginCall from './src/methods/login';
-import {getConfig} from './src/utils/getConfig';
+import { loginCall } from './src/methods/loginCall';
+import { getConfig } from './src/utils/getConfig';
 import { KarmaVendor } from './src/KarmaVendor';
 
 function getKarmaVendorInstance(env, loginId, password){
    console.log(env);
-    console.log(loginId);
-    console.log(password);
+   console.log(loginId);
+   console.log(password);
    const config = getConfig(env);
    console.log(config);
    const loginResult = loginCall(config, loginId, password);
